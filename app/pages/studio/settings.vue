bb2f33c015a89c2e2ba6eab041fd0c2fdca59b69
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-05-01 15:28:27 +0700

    feat: create studio settings page layout with navigation menu

--- app/pages/studio/settings.vue
+++ app/pages/studio/settings.vue
@@ -1,37 +1,37 @@
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
 definePageMeta({
   layout: 'studio',
   middleware: 'studio-auth'
 })
 
-const links = [
-  [{
-    label: 'Profile',
-    icon: 'i-lucide-user',
-    to: '/studio/settings',
-    exact: true
-  }, {
-    label: 'Site Management',
-    icon: 'i-lucide-globe',
-    to: '/studio/settings/site'
-  }, {
-    label: 'Members',
-    icon: 'i-lucide-users',
-    to: '/studio/settings/members'
-  }, {
-    label: 'Notifications',
-    icon: 'i-lucide-bell',
-    to: '/studio/settings/notifications'
-  }, {
-    label: 'Security',
-    icon: 'i-lucide-lock',
-    to: '/studio/settings/security'
-  }]
-]
+const links = [[{
+  label: 'Profile',
+  icon: 'i-lucide-user',
+  to: '/studio/settings',
+  exact: true
+}, {
+  label: 'Site Management',
+  icon: 'i-lucide-globe',
+  to: '/studio/settings/site'
+}, {
+  label: 'Members',
+  icon: 'i-lucide-users',
+  to: '/studio/settings/members'
+}, {
+  label: 'Notifications',
+  icon: 'i-lucide-bell',
+  to: '/studio/settings/notifications'
+}, {
+  label: 'Security',
+  icon: 'i-lucide-shield',
+  to: '/studio/settings/security'
+}]] satisfies NavigationMenuItem[][]
 </script>
 
 <template>
-  <UDashboardPanel id="settings">
+  <UDashboardPanel id="settings" :ui="{ body: 'lg:py-12' }">
     <template #header>
       <UDashboardNavbar title="Settings">
         <template #leading>
@@ -39,13 +39,13 @@ const links = [
         </template>
       </UDashboardNavbar>
 
-      <UDashboardToolbar class="py-0 px-1.5 overflow-x-auto">
-        <UHorizontalNavigation :links="links" />
+      <UDashboardToolbar>
+        <UNavigationMenu :items="links" highlight class="-mx-1 flex-1" />
       </UDashboardToolbar>
     </template>
 
     <template #body>
-      <div class="p-8">
+      <div class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full lg:max-w-2xl mx-auto">
         <NuxtPage />
       </div>
     </template>
